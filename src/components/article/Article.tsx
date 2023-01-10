408ba26678c6cf60370ba0c762edaf499a3f1079
BiancaGlavan <bianca.glavan@yahoo.ro>
2023-01-10 19:08:42 +0200

    add style to article

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,42 +1,74 @@
-import { Card, CardActions, CardContent, CardMedia, IconButton, Typography } from "@mui/material";
+import { Box, Card, CardActions, CardContent, CardMedia, IconButton, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import { IArticle } from "../../redux/features/apiSlice";
 import { baseURL } from "../../utils/config";
+import parse from "html-react-parser";
 
 interface IPropsArticle {
   article: IArticle;
 }
 
-const StyledArticle = styled('div')``;
+// ${props => props.theme.breakpoints.down("sm")} {
 
-const Article = ({article}: IPropsArticle) => {
+// }
+
+const StyledArticle = styled("div")`
+  margin-bottom: 30px;
+
+  .article-card {
+    max-width: 348px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .article-img {
+    width: 100%;
+    height: 250px;
+    object-fit: cover;
+    margin-bottom: 20px;
+  }
+
+  .truncate {
+    max-width: 100%;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-align: center;
+  }
+
+  .article-cat {
+    text-transform: uppercase;
+    text-align: center;
+  }
+
+  .article-title {
+    text-align: center;
+  }
+`;
+
+const Article = ({ article }: IPropsArticle) => {
   return (
     <StyledArticle className="Article">
-       <Card sx={{ maxWidth: 345 }}>
-        <CardMedia image={'./default-thumbnail.jpg'} />
-      <img style={{width: '100%'}} src={article.image ? baseURL+article.image : './default-thumbnail.jpg'} alt="" />
+      <Box className="article-card">
+        <img className="article-img" src={article.image ? baseURL + article.image : "./default-thumbnail.jpg"} />
 
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {article.title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary" dangerouslySetInnerHTML={{__html: article.description.slice(0, 40)}}>
-        </Typography>
-
-        <Typography variant="h6" color="text.secondary">
+        <Typography className="article-cat" variant="subtitle2" color="text.secondary">
           {article.category.title}
         </Typography>
-      </CardContent>
-      <CardActions>
-        <IconButton>
-          <FavoriteBorderOutlinedIcon />
-        </IconButton>
-      </CardActions>
-    </Card>
-       
+
+        <Typography className="article-title"  gutterBottom variant="h5" component="div">
+          {article.title}
+        </Typography>
+        {/* <Typography variant="body2" color="text.secondary" className="truncate">
+          {parse(article.description)}
+        </Typography> */}
+        <Typography className="article-cat" variant="subtitle2" color="text.secondary">
+          by {article.user.name}
+        </Typography>
+      </Box>
     </StyledArticle>
-  )
-}
+  );
+};
 
 export default Article;
