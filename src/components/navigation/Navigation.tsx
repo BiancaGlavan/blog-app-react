1b57ad23020e9f781e67b3669af555198cfa4fc6
BiancaGlavan <bianca.glavan@yahoo.ro>
2023-01-11 22:19:43 +0200

    updated navigation

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -12,12 +12,16 @@ import { logout, setUser } from "../../redux/features/authSlice";
 import UserDropdown from "./Userdropdown";
 import { useGetMyProfileQuery } from "../../redux/features/apiSlice";
 
-const StyledNavigation = styled(Container)`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 20px;
-  margin-bottom: 20px;
+const StyledNavigation = styled("div")`
+
+  .navigation-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+ 
 
   .menu-links {
     display: flex;
@@ -28,6 +32,12 @@ const StyledNavigation = styled(Container)`
       &:hover {
         color: ${(props) => props.theme.palette.secondary.main};
       }
+    }
+  }
+
+  .logo {
+    &:hover {
+      color: ${(props) => props.theme.palette.secondary.main};
     }
   }
 `;
@@ -41,7 +51,6 @@ const Navigation = () => {
   const dispatch = useAppDispatch();
 
   const { data: userProfile, isSuccess } = useGetMyProfileQuery({}, { skip: !authState.isAuth });
-
 
   const navigate = useNavigate();
 
@@ -58,66 +67,70 @@ const Navigation = () => {
     if (userProfile && userProfile.profile) {
       dispatch(setUser(userProfile.profile));
 
-    console.log('userProfile', userProfile);
+      console.log("userProfile", userProfile);
     }
   }, [userProfile]);
-  
+
   return (
-    <StyledNavigation>
-      {isMobile && (
-        <IconButton onClick={handleDrawerToggle}>
-          <MenuIcon />
-        </IconButton>
-      )}
-      {!isMobile && (
-        <Link to={"/"}>
-          <Typography>BB</Typography>
-        </Link>
-      )}
-      <Box className="menu-links">
-        {!isMobile && (
-          <>
-            <Link to={"/articles"}>
-              <Typography className="menu-link" variant="subtitle1">
-                Articles
-              </Typography>
-            </Link>
-            <Link to={"/categories"}>
-              <Typography className="menu-link" variant="subtitle1">
-                Categories
-              </Typography>
-            </Link>
-            {!authState.isAuth && (
-              <>
-                <Login />
-                <Register />
-              </>
-            )}
-          </>
+    <StyledNavigation className="Navigation">
+      <Container className="navigation-container">
+        {isMobile && (
+          <IconButton onClick={handleDrawerToggle}>
+            <MenuIcon />
+          </IconButton>
         )}
-        {authState.isAuth && authState.user && <UserDropdown user={authState.user} handleLogout={handleLogout} />}
-        {authState.isAuth && authState.user && <Link to={"/editor"}>
-          <Tooltip title="Write">
-            <IconButton>
-              <CreateIcon />
-            </IconButton>
-          </Tooltip>
-        </Link>}
-      </Box>
-      <Drawer
-        variant="temporary"
-        open={mobileOpen}
-        onClose={handleDrawerToggle}
-        ModalProps={{
-          keepMounted: true,
-        }}
-        sx={{
-          display: { xs: "block", md: "none" },
-          "& .MuiDrawer-paper": { boxSizing: "border-box", width: 280 },
-        }}
-      >
-        <SidebarNav />
-      </Drawer>
+        {!isMobile && (
+          <Link to={"/"}>
+            <Typography className="logo">Keep the Pot Boiling</Typography>
+          </Link>
+        )}
+        <Box className="menu-links">
+          {!isMobile && (
+            <>
+              <Link to={"/articles"}>
+                <Typography className="menu-link" variant="subtitle1">
+                  Articles
+                </Typography>
+              </Link>
+              <Link to={"/categories"}>
+                <Typography className="menu-link" variant="subtitle1">
+                  Categories
+                </Typography>
+              </Link>
+              {!authState.isAuth && (
+                <>
+                  <Login />
+                  <Register />
+                </>
+              )}
+            </>
+          )}
+          {authState.isAuth && authState.user && <UserDropdown user={authState.user} handleLogout={handleLogout} />}
+          {authState.isAuth && authState.user && (
+            <Link to={"/editor"}>
+              <Tooltip title="Write">
+                <IconButton>
+                  <CreateIcon />
+                </IconButton>
+              </Tooltip>
+            </Link>
+          )}
+        </Box>
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", md: "none" },
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: 280 },
+          }}
+        >
+          <SidebarNav />
+        </Drawer>
+      </Container>
     </StyledNavigation>
   );
 };
