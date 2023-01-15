8a56e98b43a618102205eda577ff22d2e079f928
BiancaGlavan <bianca.glavan@yahoo.ro>
2023-01-15 19:48:27 +0200

    added title to articles page

--- src/pages/ArticlesPage.tsx
+++ src/pages/ArticlesPage.tsx
@@ -1,10 +1,17 @@
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import ArticlesList from "../components/article/ArticlesList";
 import { useGetArticlesQuery } from "../redux/features/apiSlice";
 
 const StyledArticlePage = styled(Container)`
 margin-top: 80px;
+
+.articles-feed {
+  text-align: center;
+  margin-bottom: 50px;
+  font-weight: 600;
+  color: ${(props) => props.theme.palette.text.primary};
+}
 `;
 
 
@@ -13,6 +20,7 @@ const ArticlesPage = () => {
 
   return (
     <StyledArticlePage className="ArticlesPage">
+      <Typography className="articles-feed" variant="h5">The Feed</Typography>
       {!isLoading && articlesResponse && <ArticlesList articles={articlesResponse.articles}/>}
     </StyledArticlePage>
   )
