a9390109b7c5ee77acf6b67273114253113a5fea
BiancaGlavan <bianca.glavan@yahoo.ro>
2023-01-24 14:16:28 +0200

    added buttons for pages in admin sidebar

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -1,13 +1,54 @@
+import { Button } from "@mui/material";
 import { styled } from "@mui/material/styles";
+import ArticleIcon from "@mui/icons-material/Article";
+import HomeIcon from "@mui/icons-material/Home";
+import CategoryIcon from "@mui/icons-material/Category";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import { Link } from "react-router-dom";
 
-const StyledAdminSidebar = styled('div')``;
+const StyledAdminSidebar = styled("div")`
+  width: 280px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  .sidebar-btn {
+    display: flex;
+    justify-content: flex-start;
+    gap: 10px;
+    width: 90%;
+  }
+
+  .link {
+    width: 100%;
+  }
+`;
 
 const AdminSidebar = () => {
   return (
-    <StyledAdminSidebar>Sidebar
-      
+    <StyledAdminSidebar className="AdminSidebar">
+      <Link className="link" to={"/admin"}>
+        <Button className="sidebar-btn" startIcon={<HomeIcon />}>
+          Dashboard
+        </Button>
+      </Link>
+      <Link className="link" to={"/admin/categories"}>
+        <Button className="sidebar-btn" startIcon={<CategoryIcon />}>
+          Categories
+        </Button>
+      </Link>
+      <Link className="link" to={"/admin/articles"}>
+        <Button className="sidebar-btn" startIcon={<ArticleIcon />}>
+          Articles
+        </Button>
+      </Link>
+      <Link className="link" to={"/admin/users"}>
+        <Button className="sidebar-btn" startIcon={<AccountBoxIcon />}>
+          Users
+        </Button>
+      </Link>
     </StyledAdminSidebar>
-  )
-}
+  );
+};
 
 export default AdminSidebar;
