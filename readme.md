41bfe939ca2fa8646359ce78f3a111eb265d905d
BiancaGlavan <bianca.glavan@yahoo.ro>
2023-02-17 15:59:42 +0200

    added read me file

---
+++ readme.md
@@ -0,0 +1,33 @@
+### Links
+
+- Live Site URL: [biancaglavan.github.io/portfolio/](https://biancaglavan.github.io/portfolio/)
+
+### Screenshot
+
+![](./public/images/Capture1.PNG)
+![](./public/images/Capture2.PNG)
+
+
+### About the blog
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover and active states for all interactive elements on the page
+- Usage of Unsplash API to search for images and integrate them into the article.
+- Users can create articles, like articles, leave comments on articles.
+- Admin page where admin can create, edit and delete categories and articles.
+- Back-end for register, login, profile, categories, articles, comments, likes.
+
+### Built with
+
+- React
+- TypeScript
+- Material UI
+- Unsplash API
+- Redux Toolkit
+- React Router
+- Express js
+- MongoDB
+- Mongoose
+
+
+
--- src/components/homepage/categories.tsx
+++ src/components/homepage/categories.tsx
@@ -1,45 +1,113 @@
 import { styled } from "@mui/material/styles";
-import { Box, Button } from "@mui/material";
+import { Box, Button, IconButton, Menu } from "@mui/material";
 import { useGetCategoriesQuery } from "../../redux/features/apiSlice";
 import { Link } from "react-router-dom";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { useState } from "react";
 
 const StyledCategories = styled("div")`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
 
-    ${(props) => props.theme.breakpoints.down('sm')} {
-      gap: 5px;
+  ${(props) => props.theme.breakpoints.down("sm")} {
+    gap: 5px;
+  }
+
+  .cat {
+    color: black;
+    font-weight: 600;
+    font-size: 15px;
+
+    ${(props) => props.theme.breakpoints.down("sm")} {
+      font-size: 14px;
     }
 
-    .cat {
-      color: black;
-      font-weight: 600;
-      font-size: 15px;
-
-      ${(props) => props.theme.breakpoints.down('sm')} {
-        font-size: 14px;
-      }
-
-      &:hover {
-        color: ${(props) => props.theme.palette.primary.main};
-      }
+    &:hover {
+      color: ${(props) => props.theme.palette.primary.main};
     }
- 
+  }
+`;
 
+const StyledCategoryMenu = styled(Menu)`
+  .cat {
+    color: black;
+    font-weight: 600;
+    font-size: 15px;
+
+    ${(props) => props.theme.breakpoints.down("sm")} {
+      font-size: 14px;
+    }
+
+    &:hover {
+      color: ${(props) => props.theme.palette.primary.main};
+    }
+  }
 `;
 
 const Categories = () => {
   const { data: categories, isLoading } = useGetCategoriesQuery();
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
     <StyledCategories className="Categories">
-      {categories?.map((category) => (
+      {categories &&
+        categories?.slice(0, 3).map((category) => (
           <Link key={category._id} to={`/categories/${category._id}/articles`}>
-              <Button className="cat"> {category.title}</Button>
+            <Button className="cat"> {category.title}</Button>
           </Link>
-      ))}
+        ))}
+      {categories && categories.length > 3 && (
+        <>
+          <Box sx={{ flexGrow: 0 }}>
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, margin: "0 10px" }}>
+              <MoreVertIcon />
+            </IconButton>
+            <StyledCategoryMenu
+              sx={{ mt: "50px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              <Box
+                className="dropdown-cat"
+                sx={{
+                  padding: "10px 20px",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  gap: "10px",
+                }}
+              >
+                {categories.slice(3).map((category) => (
+                  <Link key={category._id} to={`/categories/${category._id}/articles`}>
+                    <Button className="cat"> {category.title}</Button>
+                  </Link>
+                ))}
+              </Box>
+            </StyledCategoryMenu>
+          </Box>
+        </>
+      )}
     </StyledCategories>
   );
 };
